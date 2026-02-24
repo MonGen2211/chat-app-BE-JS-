e967bda4857193349f98fd708a19b80f3e34f60b
hosonvu <vuho2211@gmail.com>
2026-02-24 09:16:18 +0700

    update code to fix bug cloudinary

--- src/server.js
+++ src/server.js
@@ -11,7 +11,6 @@ import { app, server } from "./lib/socket.js";
 
 dotenv.config();
 
-app.use(express.json());
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ limit: "10mb", extended: true }));
 app.use(cookieParser());
